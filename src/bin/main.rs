27e21388d6bd7553975d97aef90efd9debd695f9
Erwan Glasziou <wan.glasziou@gmail.com>
2023-07-04 20:04:35 +0200

    main update

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,14 +2,23 @@ use math_engine::expression::Expression;
 use std::str::FromStr;
 
 fn main() {
-    let expr = Expression::from_str("x * x * x").unwrap();
-    println!("{}", expr);
+    let expr = Expression::from_str("x * (x * 2.0) + x + 123.45").unwrap();
+    println!("initial: {}", expr);
 
     let deriv = expr.derivative("x");
-    println!("{}", deriv);
+    println!("derivative: {}", deriv);
+
+    let fact = expr.factorize().unwrap();
+    println!("factorized initial: {}", fact);
 
     let simp = deriv.factorize().unwrap();
-    println!("{}", simp);
+    println!("factorized derivative: {}", simp);
+
+    let deriv = fact.derivative("x");
+    println!("derived factorized: {}", deriv);
+
+    let fact = deriv.factorize().unwrap();
+    println!("factorized again: {}", fact);
 
     let expr2 = Expression::parse("z / 4.0").unwrap();
     let expr = expr + expr2;
